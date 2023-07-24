fb5b704cd83f0cf79483651a4148b5d7e6b7f6fb
mohamad ramdan firdaus <mramdanf@gmail.com>
2023-07-24 15:57:22 +0700

    init sequelize

---
+++ database-service/config/config.js
@@ -0,0 +1,34 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const { 
+  DB_USERNAME,
+  DB_HOSTNAME,
+  DB_NAME,
+  DB_DIALECT 
+} = process.env
+
+module.exports = {
+  "development": {
+    "username": `${DB_USERNAME}`,
+    "password": null,
+    "database": `${DB_NAME}`,
+    "host": `${DB_HOSTNAME}`,
+    "dialect": `${DB_DIALECT}`
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
---
+++ database-service/migrations/20230724084620-create-users-table.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.createTable('users', { 
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      firstName: Sequelize.STRING,
+      lastName: Sequelize.STRING,
+      emial: Sequelize.STRING(100),
+      birthDayDate: Sequelize.DATE,
+      location: Sequelize.STRING(100),
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+    */
+    await queryInterface.dropTable('users');
+  }
+};
