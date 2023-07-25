b0fa198ac540e6e72beab8a6bb19915ae047620f
mohamad ramdan firdaus <mramdanf@gmail.com>
2023-07-26 06:29:36 +0700

    move some functions

--- database-service/src/app.js
+++ database-service/src/app.js
@@ -2,10 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const cron = require('node-cron');
 const {
-  sendBirthDayMessage,
-  resendMessageOnError
-} = require('./utils/send-message.utils');
-const { checkCronJobFinished } = require('./utils/cron.utils');
+  cronSendBirthDayMessage,
+  cronResendMessageOnError
+} = require('./utils/cron-job.utils');
 
 const usersRouter = require('./routes/user');
 
@@ -14,36 +13,10 @@ const app = express();
 app.use(express.json());
 app.use('/users', usersRouter);
 
+cron.schedule('*/10 * * * * *', cronSendBirthDayMessage);
+cron.schedule('*/10 * * * * *', cronResendMessageOnError);
+
 const port = process.env.APP_PORT || 3000;
-
-cron.schedule('*/10 * * * * *', async () => {
-  const {
-    sendBirthDayMessage: { finished }
-  } = await checkCronJobFinished();
-  if (!finished) {
-    console.log('cancel sending message, prev job not finished yet.');
-    return;
-  }
-  console.log('---------------------');
-  console.log('send birthday message scheduler running...');
-  sendBirthDayMessage(4);
-});
-
-cron.schedule('*/10 * * * * *', async () => {
-  const {
-    resendingMessageOnError: { finished }
-  } = await checkCronJobFinished();
-  if (!finished) {
-    console.log(
-      'cancel resending message on error, prev job not finished yet.'
-    );
-    return;
-  }
-  console.log('---------------------');
-  console.log('resend message scheduler running...');
-  resendMessageOnError();
-});
-
 app.listen(port, () => {
   console.log(`server listening on http://localhost:${port}`);
 });
---
+++ database-service/src/utils/cron-job.utils.js
@@ -0,0 +1,38 @@
+const { checkCronJobFinished } = require('./cron.utils');
+const {
+  sendBirthDayMessage,
+  resendMessageOnError
+} = require('./send-message.utils');
+
+async function cronSendBirthDayMessage() {
+  const {
+    sendBirthDayMessage: { finished }
+  } = await checkCronJobFinished();
+  if (!finished) {
+    console.log('cancel sending message, prev job not finished yet.');
+    return;
+  }
+  console.log('---------------------');
+  console.log('send birthday message scheduler running...');
+  sendBirthDayMessage(4);
+}
+
+async function cronResendMessageOnError() {
+  const {
+    resendingMessageOnError: { finished }
+  } = await checkCronJobFinished();
+  if (!finished) {
+    console.log(
+      'cancel resending message on error, prev job not finished yet.'
+    );
+    return;
+  }
+  console.log('---------------------');
+  console.log('resend message scheduler running...');
+  resendMessageOnError();
+}
+
+module.exports = {
+  cronSendBirthDayMessage,
+  cronResendMessageOnError
+};
