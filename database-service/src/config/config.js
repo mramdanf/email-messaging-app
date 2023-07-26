0523c09e8f75cfd16399c0c0b198f5b1dcfc2bcb
mohamad ramdan firdaus <mramdanf@gmail.com>
2023-07-27 05:20:56 +0700

    dockerize

--- database-service/src/config/config.js
+++ database-service/src/config/config.js
@@ -1,14 +1,16 @@
 require('dotenv').config();
 
-const { DB_USERNAME, DB_HOSTNAME, DB_NAME, DB_DIALECT } = process.env;
+const { DB_USERNAME, DB_HOSTNAME, DB_NAME, DB_PASSWORD, DB_DIALECT, DB_PORT } =
+  process.env;
 
 module.exports = {
   development: {
-    username: `${DB_USERNAME}`,
-    password: null,
-    database: `${DB_NAME}`,
-    host: `${DB_HOSTNAME}`,
-    dialect: `${DB_DIALECT}`,
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOSTNAME,
+    dialect: DB_DIALECT,
+    port: DB_PORT,
     logQueryParameters: true,
     timezone: '+07:00',
     logging: false
