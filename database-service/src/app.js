ab76c93066c510f0c7bfbe8088f6bc5a0636116b
mohamad ramdan firdaus <mramdanf@gmail.com>
2023-07-26 19:39:16 +0700

    email messaging app swagger

--- database-service/src/app.js
+++ database-service/src/app.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const cron = require('node-cron');
+const fs = require('fs');
+const YAML = require('yamljs');
+const path = require('path');
+const swaggerUi = require('swagger-ui-express');
 const {
   cronSendBirthDayMessage,
   cronResendMessageOnError
@@ -10,8 +14,13 @@ const usersRouter = require('./routes/user');
 
 const app = express();
 
+const swaggerFilePath = path.join(process.cwd(), 'src', 'swagger.yaml');
+const swaggerFile = fs.readFileSync(swaggerFilePath, 'utf8');
+const swaggerDocument = YAML.parse(swaggerFile);
+
 app.use(express.json());
 app.use('/users', usersRouter);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 cron.schedule('*/10 * * * * *', cronSendBirthDayMessage);
 cron.schedule('*/15 * * * * *', cronResendMessageOnError);
