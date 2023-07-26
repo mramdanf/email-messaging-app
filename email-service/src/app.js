d4e419438b2c262babebbc5fcccc7a921b0da46a
mohamad ramdan firdaus <mramdanf@gmail.com>
2023-07-26 21:08:22 +0700

    swagger for mail service

--- email-service/src/app.js
+++ email-service/src/app.js
@@ -1,12 +1,20 @@
 require('dotenv').config();
 const express = require('express');
 const timeout = require('connect-timeout');
+const fs = require('fs');
+const YAML = require('yamljs');
+const path = require('path');
+const swaggerUi = require('swagger-ui-express');
 
 const mainRouter = require('./routes');
 
 function haltOnTimedout(req, res, next) {
   if (!req.timedout) next();
 }
+
+const swaggerFilePath = path.join(process.cwd(), 'src', 'swagger.yaml');
+const swaggerFile = fs.readFileSync(swaggerFilePath, 'utf8');
+const swaggerDocument = YAML.parse(swaggerFile);
 
 const app = express();
 
@@ -15,6 +23,7 @@ app.use(express.json());
 app.use(haltOnTimedout);
 app.use('/', mainRouter);
 app.use(haltOnTimedout);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const port = process.env.APP_PORT || 3001;
 app.listen(port, () => {
